e3ca69632d2438e58132d7348abedaa1f9c97313
Kafolabi <koladeafolabi2020@gmail.com>
2025-02-12 01:21:11 +0100

    Update styles and layout in App component
    - Modified App.css to improve the overall styling and layout
    - Updated App.jsx to reflect the new design changes

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,31 @@ import Calculator from "./components/Calculator.jsx";
 
 function App() {
   return (
-    <section className="mx-20 min-w-fit h-full flex my-16 max-md:mx-0 max-md:my-0">
-      <section className="flex flex-col md:flex-row h-full relative">
-        <div className="absolute inset-0 bg-white md:rounded-tl-3xl md:rounded-bl-3xl overflow-hidden">
-          <div className="w-1/2 h-full bg-main md:rounded-bl-[6rem] md:rounded-tl-3xl absolute right-0"></div>
-        </div>
-        <Calculator />
+    <>
+      <section className="mx-20 min-w-fit h-full flex mt-16 max-md:mx-0 max-md:my-0">
+        <section className="flex flex-col md:flex-row h-full relative">
+          <div className="absolute inset-0 bg-white md:rounded-tl-3xl md:rounded-bl-3xl overflow-hidden">
+            <div className="w-1/2 h-full bg-main md:rounded-bl-[6rem] md:rounded-tl-3xl absolute right-0"></div>
+          </div>
+          <Calculator />
+        </section>
       </section>
-    </section>
+
+      <div className="attribution mt-2">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="https://github.com/Kafolabi/mortgage-repayment-app">
+          Kolade Afolabi
+        </a>
+        .
+      </div>
+      <footer className="mt-8">
+        <p>© 2025 Kolade Afolabi. All rights reserved.</p>
+      </footer>
+    </>
   );
 }
 
